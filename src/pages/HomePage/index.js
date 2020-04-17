c574ec6c749a4559e885f437c16a9671b809f9c3
Goksen <goksengorgulu@gmail.com>
2020-04-17 12:45:55 +0200

    build search by language&distance forms and create simple action for search

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,6 +4,10 @@ import SearchBox from "../../components/SearchBox";
 export default function HomePage() {
   return (
     <div>
+      <h5>
+        Search a book by it's title or check out the books in your language or
+        just browse the books around you!
+      </h5>
       <SearchBox />
     </div>
   );
