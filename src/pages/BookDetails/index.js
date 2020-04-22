0df4d5eb06815435dc4b6cdc60369b4c206ab5d1
Goksen <goksengorgulu@gmail.com>
2020-04-22 17:47:05 +0200

    fetch book details by id & update state in the reducer

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getBookById } from "../../store/book/actions";
+import { useEffect } from "react";
+import { selectBookDetails } from "../../store/book/selectors";
 import Book from "../../components/Book";
 
 export default function BookDetails() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const book = useSelector(selectBookDetails);
+  console.log("BOOK in BOOKDETAILS", book);
+
+  useEffect(() => {
+    dispatch(getBookById(id));
+  }, [dispatch, id]);
+
   return (
     <div>
-      BOOK DETAILS
-      <Book />
+      <Book
+        key={book.id}
+        id={book.id}
+        title={book.title}
+        description={book.description}
+        imageUrl={book.imageUrl}
+        isAvailable={book.isAvailable}
+        author={book.author}
+      />
     </div>
   );
 }
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
 import books from "./searchResult/reducer";
+import bookDetails from "./book/reducer";
 
 export default combineReducers({
   appState,
   user,
-  books
+  books,
+  bookDetails
 });
